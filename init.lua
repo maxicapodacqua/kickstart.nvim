158f6c7acf0e9edf630208861edbe34e82cceda3
Maxi Capodacqua <maximiliano.capodacqua@strategiced.com>
2024-06-05 16:45:14 -0500

    Adding win separator

--- init.lua
+++ init.lua
@@ -967,6 +967,11 @@ require('lazy').setup({
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
   { import = 'custom.plugins' },
+  {
+    'nvim-zh/colorful-winsep.nvim',
+    config = true,
+    event = { 'WinNew' },
+  },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
